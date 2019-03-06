8258b3da414aee8e9fd79c26f7401d556aca6ef6
Ju Chan-jong <juchan-jong@juui-imac.local>
2019-03-06 21:12:42 +0900

    Initial Commit

---
+++ Xcode Break point/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Xcode Break point
+//
+//  Created by Ju Chan-jong on 2019. 3. 3..
+//  Copyright © 2019년 Ju Chan-jong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var sum = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var count = 0
+        for row in 5...10 {
+            count += 1
+            self.sum += row
+        }
+        
+        print("Total Sum \(self.sum), \(count) times")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
+
